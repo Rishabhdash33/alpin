87c488045ce61bdaa61f7c6396526c3c46bdeb98
Rishabhdash33 <rishabhparikh21@gmail.com>
2025-06-04 16:08:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -385,5 +385,5 @@ def update_attendance_table(month, processor):
     return filtered_df.to_dict('records')
 
 # Run the app
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8050)
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=10000)
